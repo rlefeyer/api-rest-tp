62f6d24a100449539d6939744f48f3bcdfb731a4
Max Becker <beckermaxime84@gmail.com>
2023-02-26 16:22:01 +0100

    amelioration api

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,87 +1,94 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ArticlesService } from './articles.service';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
-import { ApiResponse } from '@nestjs/swagger';
-import { Article } from './entities/article.entity';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {ArticlesService} from './articles.service';
+import {CreateArticleDto} from './dto/create-article.dto';
+import {UpdateArticleDto} from './dto/update-article.dto';
+import {ApiResponse} from '@nestjs/swagger';
+import {Article} from './entities/article.entity';
+import {Throttle} from '@nestjs/throttler';
 
 @Controller('articles')
 export class ArticlesController {
-  constructor(private readonly articlesService: ArticlesService) {}
+    constructor(private readonly articlesService: ArticlesService) {
+    }
 
-  @Post()
-  @ApiResponse({
-    status: 200,
-    description: 'Création de l\'article',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Page introuvable',
-  })
-  create(@Body() createArticleDto: CreateArticleDto) {
-    const entity = this.convertDtoToEntity(createArticleDto);
-    return this.articlesService.create(entity);
-  }
+    @Throttle(10, 60)
+    @Post()
+    @ApiResponse({
+        status: 200,
+        description: 'Création de l\'article',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Page introuvable',
+    })
+    create(@Body() createArticleDto: CreateArticleDto) {
+        const entity = this.convertDtoToEntity(createArticleDto);
+        return this.articlesService.create(entity);
+    }
 
-  @Get()
-  @ApiResponse({
-    status: 200,
-    description: 'Voici tous les articles',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Page introuvable',
-  })
-  findAll() {
-    return this.articlesService.findAll();
-  }
+    @Get()
+    @ApiResponse({
+        status: 200,
+        description: 'Voici tous les articles',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Page introuvable',
+    })
+    @ApiResponse({
+        status: 403,
+        description: "Vous n'aviez pas les autorisations nécessaires pour accéder à la ressource demandée."
+    })
+    findAll() {
+        return this.articlesService.findAll();
+    }
 
-  
-  @Get(':id')
-  @ApiResponse({
-    status: 200,
-    description: 'Voici l\'article demandé',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Page introuvable',
-  })
-  findOne(@Param('id') id: string) {
-    return this.articlesService.findOne(id);
-  }
 
-  @Patch(':id')
-  @ApiResponse({
-    status: 200,
-    description: 'Article modifié',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Page introuvable',
-  })
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articlesService.update(id, updateArticleDto);
-  }
+    @Get(':id')
+    @ApiResponse({
+        status: 200,
+        description: 'Voici l\'article demandé',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Page introuvable',
+    })
+    findOne(@Param('id') id: string) {
+        return this.articlesService.findOne(id);
+    }
 
-  @Delete(':id')
-  @ApiResponse({
-    status: 200,
-    description: 'suppression de l\'article demandé',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Page introuvable',
-  })
-  remove(@Param('id') id: string) {
-    return this.articlesService.remove(id);
-  }
+    @Patch(':id')
+    @ApiResponse({
+        status: 200,
+        description: 'Article modifié',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Page introuvable',
+    })
+    update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+        return this.articlesService.update(id, updateArticleDto);
+    }
 
-  private convertDtoToEntity(dto: CreateArticleDto): Article {
-    const article = new Article();
-    article.name = dto.name;
-    article.quantity = dto.quantity;
-    article.price = dto.price;
-    article.storeId = dto.storeId;
-    return article;
-  }
+    @Delete(':id')
+    @ApiResponse({
+        status: 200,
+        description: 'suppression de l\'article demandé',
+    })
+    @ApiResponse({
+        status: 404,
+        description: 'Page introuvable',
+    })
+    remove(@Param('id') id: string) {
+        return this.articlesService.remove(id);
+    }
+
+    private convertDtoToEntity(dto: CreateArticleDto): Article {
+        const article = new Article();
+        article.name = dto.name;
+        article.quantity = dto.quantity;
+        article.price = dto.price;
+        article.storeId = dto.storeId;
+        return article;
+    }
 }
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OrdersService } from './orders.service';
+import { AuthService } from './auth.service';
 
-describe('OrdersService', () => {
-  let service: OrdersService;
+describe('AuthService', () => {
+  let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [AuthService],
     }).compile();
 
-    service = module.get<OrdersService>(OrdersService);
+    service = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
