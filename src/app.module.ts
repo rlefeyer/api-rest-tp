dc571adb90661e93f76a73feaaf16efa7a080880
Theo delaporte <theodelaporte@MacBook-Pro-de-Theo.local>
2023-02-27 09:42:25 +0100

    fix compilation error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { StoresModule } from './stores/stores.module';
-import { ArticlesModule } from './articles/articles.module';
-import { OrdersModule } from './orders/orders.module';
-import { StatusModule } from './status/status.module';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {StoresModule} from './stores/stores.module';
+import {ArticlesModule} from './articles/articles.module';
+import {OrdersModule} from './orders/orders.module';
+import {StatusModule} from './status/status.module';
+import {ThrottlerGuard, ThrottlerModule} from '@nestjs/throttler';
+import {APP_GUARD} from '@nestjs/core';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from './users/users.module';
 
 
 @Module({
-  imports: [StoresModule, ArticlesModule, OrdersModule, StatusModule, ThrottlerModule.forRoot({ ttl: 60, limit: 10, }), AuthModule, UsersModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [StoresModule, ArticlesModule, OrdersModule, StatusModule, ThrottlerModule.forRoot({
+        ttl: 60,
+        limit: 10,
+    }), AuthModule, UsersModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard
+        }
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
